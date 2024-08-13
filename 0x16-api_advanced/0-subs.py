3f95f291c2639af2128322d940972b59cb4c2b41
Solomon Nwante <myphilothea@gmail.com>
2024-08-13 16:32:10 +0100

    Update user-agent

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,7 @@ import requests
 def number_of_subscribers(subreddit):
     """Returns the number of subscribers for a given subreddit."""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "python-requests/2.25.1"}
+    headers = {"User-Agent": "Chrome/91.0.4472.124"}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
