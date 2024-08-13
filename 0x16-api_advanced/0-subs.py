c6ce9b51c4fc5c99ac146dc50696c3a4cddea60d
Solomon Nwante <myphilothea@gmail.com>
2024-08-13 14:27:06 +0100

    Revert to old solution

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,11 @@ def number_of_subscribers(subreddit):
     headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /CNwante)"}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    if response.status_code == 200:
+        try:
+            data = response.json()
+            return data['data']['subscribers']
+        except (ValueError, KeyError):
+            return 0
+    else:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
