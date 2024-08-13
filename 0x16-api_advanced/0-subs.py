459e5d4ffc165c8402662919a8d159085b9c76f0
Solomon Nwante <myphilothea@gmail.com>
2024-08-13 14:22:22 +0100

    Update string format style

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,7 +5,7 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Returns the number of subscribers for a given subreddit."""
-    url = f"https://www.reddit.com/r/{subreddit}/about.jsom"
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /CNwante)"}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
