67b49a2bbad9b83b1b61d52747de121cd76130d2
Solomon Nwante <myphilothea@gmail.com>
2024-08-13 14:20:51 +0100

    Fix pep8 code style error

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,15 +4,16 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-  """Returns the number of subscribers for a given subreddit."""
-  url = f"https://www.reddit.com/r/{subreddit}/about.jsom"
-  headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /CNwante)"}
+    """Returns the number of subscribers for a given subreddit."""
+    url = f"https://www.reddit.com/r/{subreddit}/about.jsom"
+    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /CNwante)"}
 
-  response = requests.get(url, headers=headers, allow_redirects=False)
-  if response.status_code == 200:
-    try:
-      data = response.json()
-      return data['data']['subscribers']
-    except (ValueError, KeyError):
-      return 0
-  else: return 0
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        try:
+            data = response.json()
+            return data['data']['subscribers']
+        except (ValueError, KeyError):
+            return 0
+    else:
+        return 0
