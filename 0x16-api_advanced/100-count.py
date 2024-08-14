a7c354f0e9ddebcfcb4b488a70c6eacd243bdf8b
Solomon Nwante <myphilothea@gmail.com>
2024-08-14 01:32:26 +0100

    Fix pycodestyle error

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -13,7 +13,11 @@ def count_words(subreddit, word_list, instances=None, after="", count=0):
     headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/CNwante)"}
     params = {"after": after, "count": count, "limit": 100}
 
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    response = requests.get(
+        url, headers=headers,
+        params=params,
+        allow_redirects=False
+    )
 
     try:
         results = response.json()
@@ -36,7 +40,8 @@ def count_words(subreddit, word_list, instances=None, after="", count=0):
 
     if after is None:
         if len(instances) > 0:
-            instances = sorted(instances.items(), key=lambda kv: (-kv[1], kv[0]))
+            instances = sorted(instances.items(),
+                               key=lambda kv: (-kv[1], kv[0]))
             for k, v in instances:
                 print("{}: {}".format(k, v))
     else:
